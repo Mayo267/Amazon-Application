20ed54eff09e951c40d74ffd81338d5a1ba5b227
Andrew Mayo <andrewmayo@mayo.fios-router.home>
2019-02-07 15:32:36 -0500

    prompt finished

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,12 +24,31 @@ connection.connect(function(err) {
 function findItem(){
     function search(){
         inquirer
-        .prompt({
+        .prompt([
+            {
             name: "find_id",
             type: "input",
             message: "Please type the list number of the item you'd like to purchase?"
-        }).then(function(user){
-            console.log("You've selected " + user.find_id + " ... ");
+            },
+            {
+            name: "quantity",
+            type: "input",
+            message: "How many will you be purchasing?"
+            },
+            {
+            name:"confirm",
+            type: "confirm",
+            message: "Is this information correct?"
+            }
+        ]
+        
+        ).then(function(user){
+            if(user.confirm === true){
+            console.log("\nYou've selected number " + user.find_id + ".");
+            console.log("\nYou're purchasing " + user.quantity + " units.");
+            }else{
+                search();
+            }
         })
     }
 search();
@@ -37,13 +56,12 @@ search();
 
 //===========================================================================//
 function showList(){
-    console.log("\nShowing List.... ");
+    console.log("\n"  +  "\nShowing List.... ");
     connection.query(
         "SELECT * FROM products",
         function(err, res) {
-            // console.log(res);
             for(var i = 0; i < res.length; i++){
-                console.log("\n" + res[i].itemID + " | " + res[i].productName + " -- " + res[i].deptName + " -- " + "$" + res[i].price + " -- (" + res[i].stockQuantity + " Left in Stock!)");
+                console.log("\n" + res[i].itemID + " | " + res[i].productName + " -- " + res[i].deptName + " -- " + "$" + res[i].price + " -- (" + res[i].stockQuantity + " Left in Stock!)" + "\n");
             }
         }
 )
